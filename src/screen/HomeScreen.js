2389482845d01f7642b93b3ce3929d60e32ff0b4
Lorenzo Éric <134990909+LrnzGit2004@users.noreply.github.com>
2025-01-20 22:56:29 +0200

    Validation HomeScreen Jour1

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -17,7 +17,7 @@ const HomeScreen = () => {
       >
         <Text style={{ fontSize: 22, fontWeight: 500 }}>Tâches</Text>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={{fontSize: 15, fontWeight: 500}}>Voir tout</Text>
+          <Text style={{ fontSize: 15, fontWeight: 500 }}>Voir tout</Text>
           <Image
             style={{ marginLeft: 6 }}
             source={require("../../assets/images/arrow.png")}
@@ -27,7 +27,54 @@ const HomeScreen = () => {
         </View>
       </View>
 
-      
+      <View style={{ alignItems: "center", marginTop: 30 }}>
+        <Image
+          style={{ marginLeft: 6, width: 150, height: 150 }}
+          source={require("../../assets/images/no_task_icon.png")}
+        />
+        <Text
+          style={{ color: "red", fontSize: 19, marginTop: 20, fontWeight: 500 }}
+        >
+          Vous n’avez aucune tâche{" "}
+        </Text>
+        <Text style={{ fontSize: 19, marginTop: 15, fontWeight: 500 }}>
+          Créez une tâche pour commencer
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          top: 200,
+          justifyContent: "space-between",
+          paddingLeft:30,
+          paddingRight:30
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Image
+            style={{ width: 30, height: 30 }}
+            source={require("../../assets/images/progress_icon.png")}
+          />
+          <Text style={{ fontSize: 15, marginTop: 10, fontWeight: 500 }}>
+            En cours
+          </Text>
+        </View>
+        <View>
+        <Image
+            style={{ width: 50, height: 50, borderRadius:50 }}
+            source={require("../../assets/images/add_icon.png")}
+          />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            style={{ width: 30, height: 30 }}
+            source={require("../../assets/images/completed_icon.png")}
+          />
+          <Text style={{ fontSize: 15, marginTop: 10, fontWeight: 500 }}>
+            Terminée(s)
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
